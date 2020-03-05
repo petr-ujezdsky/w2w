7025518bf6051a917772a01cc6682428034f5dda
Petr Újezdský <petr.ujezdsky@gmail.com>
2020-03-05 11:19:20 +0100

    Created EjbLocator.java for EJB lookup

---
+++ client/src/main/java/com/foo/EjbLocator.java
@@ -0,0 +1,56 @@
+package com.foo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.Properties;
+
+/**
+ * Locator for EJB services.
+ */
+public class EjbLocator {
+
+    private static final Logger logger = LoggerFactory.getLogger(MyServlet.class);
+
+    public static final EjbLocator INSTANCE = new EjbLocator();
+
+    private final Context ctx;
+
+    public EjbLocator() {
+        try {
+            ctx = createInitialContext();
+        } catch (NamingException e) {
+            throw new RuntimeException("Could not create JNDI context", e);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public  <T> T lookupBean(Class<T> clazz) {
+        final String appName = "server-ear";
+        final String moduleName = "server-ejb";
+        final String distinctName = "";
+        final String viewClassName = clazz.getName();
+        String beanName = clazz.getSimpleName() + "Bean";
+
+        String jndiName = "ejb:" + appName + "/" + moduleName + "/" + distinctName + "/" + beanName + "!" + viewClassName;
+
+        logger.info("Used JNDI name " + jndiName);
+        try {
+            return (T) ctx.lookup(jndiName);
+        } catch (NamingException e) {
+            throw new IllegalArgumentException("Class " + clazz.getName() + " has no EJB server counterpart", e);
+        }
+    }
+
+    private Context createInitialContext() throws NamingException {
+        Properties prop = new Properties();
+
+        prop.put(Context.INITIAL_CONTEXT_FACTORY, "org.wildfly.naming.client.WildFlyInitialContextFactory");
+        prop.put(Context.PROVIDER_URL, "http-remoting://w2w-server:8080");
+
+        return new InitialContext(prop);
+    }
+}
--- client/src/main/java/com/foo/MyServlet.java
+++ client/src/main/java/com/foo/MyServlet.java
@@ -3,8 +3,6 @@ package com.foo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,19 +11,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Properties;
 
 @WebServlet(urlPatterns = "/test")
 public class MyServlet extends HttpServlet {
-
-    private static final long serialVersionUID = 1L;
 
     private static final Logger logger = LoggerFactory.getLogger(MyServlet.class);
 
     private final MyService myServiceRemote;
 
-    public MyServlet() throws NamingException {
-        myServiceRemote = lookupBean(MyService.class);
+    public MyServlet() {
+        myServiceRemote = EjbLocator.INSTANCE.lookupBean(MyService.class);
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -39,30 +34,5 @@ public class MyServlet extends HttpServlet {
         out.flush();
         out.close();
         logger.info("Processing request - end");
-    }
-
-    @SuppressWarnings("unchecked")
-    private <T> T lookupBean(Class<T> clazz) throws NamingException {
-        Context ctx = createInitialContext();
-
-        final String appName = "server-ear";
-        final String moduleName = "server-ejb";
-        final String distinctName = "";
-        final String viewClassName = clazz.getName();
-        String beanName = clazz.getSimpleName() + "Bean";
-
-        String jndiName = "ejb:" + appName + "/" + moduleName + "/" + distinctName + "/" + beanName + "!" + viewClassName;
-
-        logger.info("Used JNDI name " + jndiName);
-        return (T) ctx.lookup(jndiName);
-    }
-
-    private Context createInitialContext() throws NamingException {
-        Properties prop = new Properties();
-
-        prop.put(Context.INITIAL_CONTEXT_FACTORY, "org.wildfly.naming.client.WildFlyInitialContextFactory");
-        prop.put(Context.PROVIDER_URL, "http-remoting://w2w-server:8080");
-
-        return new InitialContext(prop);
     }
 }
